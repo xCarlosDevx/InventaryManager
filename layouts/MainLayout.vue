6666e39ee36d3af8809b0d20750a9d73523f99db
Carlos A <lahozfrias@gmail.com>
2021-08-27 20:05:20 -0400

    a

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -12,7 +12,7 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Quasar Apps
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
@@ -48,7 +48,8 @@
 
 <script>
 import EssentialLink from 'components/EssentialLink.vue'
-
+import { useQuasar } from 'quasar'
+ const $q = useQuasar()
 const linksData = [
   {
     title: 'Docs',
@@ -99,6 +100,7 @@ export default {
   components: { EssentialLink },
   data () {
     return {
+      $q,
       leftDrawerOpen: false,
       essentialLinks: linksData
     }
